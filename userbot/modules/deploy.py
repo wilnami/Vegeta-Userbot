3032ccb90b629c5d65ea94b3d9e7686c9368ebd2
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-20 05:10:47 +0700

    added modules [```deploy```] (#823)

---
+++ userbot/modules/deploy.py
@@ -0,0 +1,10 @@
+from userbot import CMD_HELP
+
+CMD_HELP.update({
+    "deploy": get vegeta-userbot\
+    \n↳ : commad here.",
+    "deploy": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.deploy`\
+    \n↳ : get deploy heroku.",
+    "string": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.string`\
+    \n↳: Menampilkan link String Vegeta-Userbot",
+})
