c657b875c6919768ef704cb2a51dbc53c5c5103a
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-23 15:47:45 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -7,7 +7,7 @@
 
 import asyncio
 from userbot import ALIVE_NAME, CMD_HELP, REPO_NAME, EMOJI_HELP
-from userbot.events import register
+from userbot.utils import ren_cmd
 from platform import uname
 
 modules = CMD_HELP
@@ -17,23 +17,23 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.help(?: |$)(.*)")
-async def help(rambot):
+@ren_cmd(pattern="help(?: |$)(.*)")
+async def help(renbot):
     """ For .help command,"""
-    args = rambot.pattern_match.group(1).lower()
+    args = renbot.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await rambot.edit(str(CMD_HELP[args]))
+            await renbot.edit(str(CMD_HELP[args]))
         else:
-            await rambot.edit("**`NGETIK YANG BENER NGENTOT!`**")
+            await renbot.edit("**`NGETIK YANG BENER NGENTOT!`**")
             await asyncio.sleep(50)
-            await rambot.delete()
+            await renbot.delete()
     else:
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
             string += f"`\t {EMOJI_HELP}  "
-        await rambot.edit(f"**{REPO_NAME}**\n\n"
+        await renbot.edit(f"**{REPO_NAME}**\n\n"
                          f"**{EMOJI_HELP} 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**{EMOJI_HELP}  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
                          f"**{EMOJI_HELP} 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ══════════╣❃ ♕ ❃╠══════════\n\n"
                          f"{EMOJI_HELP} {string}\n\n ══════════╣❃ ♕ ❃╠══════════\n\nSupport @notsupports\n\n")
